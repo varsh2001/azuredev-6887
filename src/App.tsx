7b67ebbb90a182c483c96cb167c56bfe0b14be1b
varsh2001 <varshithchandramukhi1@gmail.com>
2025-11-09 23:17:24 -0500

    Now works on both local and deployment testing.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ interface Message {
   timestamp: Date;
 }
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL||"https://localhost:4000";
 
 async function sendChatMessageToBackend(message: string) {
   const res = await fetch(`${API_URL}/api/chat`, {
